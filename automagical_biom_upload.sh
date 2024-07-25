1a3ee4418f31cad1b2e41d4de0272d2ec7f59eb5
Danielle Callan <dcallan@upenn.edu>
2024-07-25 13:13:33 -0400

    sigh

--- automagical_biom_upload.sh
+++ automagical_biom_upload.sh
@@ -1,11 +1,9 @@
-## this bearer token is from my (Danielle's) user account. 
-## Replace it w yours, or give me free data
-## find yours in devtools when you visit the user dataset space
+## find your bearer token in devtools when you visit the user dataset space
 
 for file in `ls biom/ | sed 's/\.biom//g'`; do 
 	curl -X POST \
 		-H "Content-type: multipart/form-data" \
-		-H 'Authorization: Bearer eyJhbGciOiJFUzUxMiJ9.eyJzdWIiOiIyMjA2ODU3NjAiLCJpc19ndWVzdCI6ZmFsc2UsImlzcyI6Imh0dHBzOi8vZXVwYXRoZGIub3JnL29hdXRoIiwiYXVkIjoiYXBpQ29tcG9uZW50U2l0ZSIsImF6cCI6ImFwaUNvbXBvbmVudFNpdGUiLCJhdXRoX3RpbWUiOjE3MjA3NTMyMjMsImlhdCI6MTcyMDc1MzIyMywiZXhwIjoxODE1MzYxMjIzLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJkY2FsbGFuLjIyMDY4NTc2MCIsInNpZ25hdHVyZSI6IjkwMTZiYzljYmY5ZGQ4MjI3NjY5ZTYxOGFlNDRmNTMyIn0.AN0waUzj4izeT6Wo_edFq0-dt48bPscbxZlUq8xlajlhWkr0cK1J_2kaEENSssdmIIzTAc5ZNZBJ_8LFg7I4AiiWAZQzG1b2hTzqgagkrbibGmLzGn9yHr9SlFMBOin4dnybTKaCcTdkjCqsQzHMTwKXCsVpPZxKWiGfiZyQAQZFWKUc' \
+		-H 'Authorization: [TODO_YOUR_BEARER_TOKEN_HERE]' \
 		-F meta='{"name":"'$file'-TEST","summary":"'$file': a study from the R package curatedMetagenomicData","description":"'$file': a study from the R package curatedMetagenomicData","datasetType":{"name":"biom","version":"1.0"},"projects":["MicrobiomeDB"],"dependencies":[],"origin":"direct-upload"}' \
 		-F file=@biom/$file.biom \
 		https://qa.microbiomedb.org/vdi/vdi-datasets; 
